c38ab9e411c2acc40fb3521c6e067bd67fb5e7cd
jeremias beta <121831975+alajeremias@users.noreply.github.com>
2023-01-03 13:26:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+nome=input('qual seu nome?')
+idade=input('qual a sua idade?')
+cor=input('qual a sua cor preferida?')
+print('ola,{}!voce tem {} de idade,e gosta da cor: {}!'.format(nome,idade,cor))
